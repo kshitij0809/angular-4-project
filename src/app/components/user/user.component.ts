d1dd413b7997e8644c7b3adf41b0b72946712d00
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-01-03 05:31:31 +0530

    user component inatilized with some list items

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  name:string;
+  age:number;
+  email:string;
+  hobbies:string[];
+  hello:any;
+  isEdit:boolean = false;
+
+  ngOnInit() {
+    console.log('ngOnInit ran...');
+
+    this.name = 'John Doe';
+    this.email = 'test@test.com';
+    this.age = 30;
+    this.address = {
+      street:'50 Main st',
+      city: 'Boston',
+      state:'MA'
+    }
+    this.hobbies = ['Write code', 'Watch movies', 'Listen to music'];
+    this.hello ='hello';
+
+   
+  }
+
+ 
+
+}
+
